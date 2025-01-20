e36da9284ef9bf5af8e0423f566063cf20093b2c
Breno <brenluz@outlook.com>
2025-01-20 14:02:54 -0300

    Turnos de batalha adicionados, Novos itens adicionados ao repositorio

--- BattleSystem/BattleManager.cs
+++ BattleSystem/BattleManager.cs
@@ -1,4 +1,7 @@
-﻿using Entities;
+﻿using System.Linq.Expressions;
+using BattleSystem.Exceptions;
+using Entities.Enemies;
+using Entities;
 
 namespace BattleSystem;
 
@@ -8,7 +11,40 @@ public class BattleManager
    {
       Console.WriteLine("Battle Started!");
       Console.WriteLine($"Player {player.Name} is fighting a {enemy.Modifier} level {enemy.Level} {enemy.Name}");
-      Console.ReadLine();
-   } 
+      
+      if(player.Speed > enemy.Speed)
+      {
+         PlayerTurn(player, enemy);
+         EnemyTurn(player, enemy);
+      }
+      else
+      {
+         EnemyTurn(player, enemy);
+         PlayerTurn(player, enemy);
+      }
+   }
+
+   private static void PlayerTurn(Player player, BaseEnemy enemy)
+   {
+      var random = new Random();
+      Console.WriteLine("Player's turn");
+      Console.WriteLine("Player attacks the enemy");
+      var attack = player.Attack * random.Next(9, 11)*0.1;
+      if (attack > enemy.Defense) 
+      {
+         enemy.Hp -= attack - enemy.Defense;
+      }   
+   }
    
+   private static void EnemyTurn(Player player, BaseEnemy enemy)
+   {
+      var random = new Random();
+      Console.WriteLine("Enemy's turn");
+      Console.WriteLine("Enemy attacks the player");
+      var attack = enemy.Attack * random.Next(9, 11)*0.1;   
+      if (attack > player.Defense) 
+      {
+         player.Hp -= enemy.Attack - player.Defense;
+      }   
+   }
 }
--- Repositories/ItemRepository.cs
+++ Repositories/ItemRepository.cs
@@ -7,8 +7,10 @@ public class ItemRepository: BaseRepository<BaseItem>
 {
     protected override IList<BaseItem> Items { get; } = new List<BaseItem>
     {
-        new BaseItem(1, "Sword", 10, Rarities.Common, 10, "A basic sword"),
-
+        new BaseItem(1, "Sword", 10, Rarities.Common,  "A basic sword"),
+        new BaseItem(2, "Shield", 10, Rarities.Common,  "A basic shield"),
+        new BaseItem(3, "Potion", 10, Rarities.Common, "A basic potion", 10),
+        new BaseItem(4, "Boots", 10, Rarities.Common, "A basic boots", 0, 0, 0, 10),
     };
 
 }
